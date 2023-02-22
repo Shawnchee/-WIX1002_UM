618ecb69e4fa5097574f3ec330eabc32d43958ba
Shawnchee <116136622+Shawnchee@users.noreply.github.com>
2023-02-22 23:37:03 +0800

    Lab1

---
+++ L1/src/l1/L1Q4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package l1;
+
+/**
+ *
+ * @author aykay
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("1 * = 100 Sales of Product A.");
+        System.out.println("January \t*************************");
+        System.out.println("February\t****************");
+        System.out.println("March   \t********************");
+        System.out.println("April   \t***************************");
+        System.out.println("May     \t*****************************\t\t\t***");
+        System.out.println("June    \t********");
+        // TODO code application logic here
+    }
+}
